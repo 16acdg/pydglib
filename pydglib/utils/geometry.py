c457e8a8667c7ea26b1c9d652a58e24b96167a88
Andrew Gray <andrew.gray@queensu.ca>
2023-08-19 14:41:14 -0400

    Created geometry module. Adding functions for evaluated geometric properties of triangles.

---
+++ pydglib/utils/geometry.py
@@ -0,0 +1,97 @@
+from typing import Union, List, Tuple
+import numpy as np
+
+
+def _is_valid_vertex(v: np.ndarray) -> bool:
+    return isinstance(v, np.ndarray) and len(v.shape) == 1 and v.shape[0] == 2
+
+
+def get_area_of_triangle(v1: np.ndarray, v2: np.ndarray, v3: np.ndarray) -> float:
+    """
+    Returns the surface area of the triangle with vertices `v1`, `v2`, `v3`.
+
+    The first coordinate of each vertex is the x position, the second is the y position.
+
+    Args:
+        v1 (np.ndarray): First vertex of the triangle.
+        v2 (np.ndarray): Second vertex of the triangle.
+        v3 (np.ndarray): Third vertex of the triangle.
+
+    Returns:
+        float: Surface area of the triangle.
+    """
+    for v in [v1, v2, v3]:
+        assert _is_valid_vertex(v)
+
+    x1, y1 = v1
+    x2, y2 = v2
+    x3, y3 = v3
+
+    area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+
+    return area
+
+
+def get_perimeter_of_triangle(v1: np.ndarray, v2: np.ndarray, v3: np.ndarray) -> float:
+    """
+    Returns the perimeter of the triangle with vertices `v1`, `v2`, `v3`.
+
+    The first coordinate of each vertex is the x position, the second is the y position.
+
+    Args:
+        v1 (np.ndarray): First vertex of a triangle.
+        v2 (np.ndarray): Second vertex of a triangle.
+        v3 (np.ndarray): Third vertex of a triangle.
+
+    Returns:
+        float: Perimeter of the triangle.
+    """
+    for v in [v1, v2, v3]:
+        assert _is_valid_vertex(v)
+
+    perimeter = sum([np.linalg.norm(e) for e in [v1 - v2, v2 - v3, v3 - v1]])
+
+    return perimeter
+
+
+def get_outward_unit_normals_of_triangle(
+    v1: np.ndarray, v2: np.ndarray, v3: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Calculates the outward unit normals of the triangle with vertices `v1`, `v2`, `v3`.
+
+    The first coordinate of each vertex is the x position, the second is the y position.
+    Assumes that the vertices are ordered counter-clockwise.
+
+    Args:
+        v1 (np.ndarray): First vertex of a triangle.
+        v2 (np.ndarray): Second vertex of a triangle.
+        v3 (np.ndarray): Third vertex of a triangle.
+
+    Returns:
+        np.ndarray: The outward unit normal of the edge `v1`-`v2`.
+        np.ndarray: The outward unit normal of the edge `v2`-`v3`.
+        np.ndarray: The outward unit normal of the edge `v3`-`v1`.
+    """
+
+    # Make vertices 3d
+    v1 = np.hstack((v1, 0))
+    v2 = np.hstack((v2, 0))
+    v3 = np.hstack((v3, 0))
+
+    # Define edges of the triangle
+    e1 = v2 - v1
+    e2 = v3 - v2
+    e3 = v1 - v3
+
+    # Find outward normals via cross products (in 3-space)
+    z = np.array([0, 0, 1])
+    n1 = np.cross(e1, z)[:2]
+    n2 = np.cross(e2, z)[:2]
+    n3 = np.cross(e3, z)[:2]
+
+    n1 = n1 / np.linalg.norm(n1)
+    n2 = n2 / np.linalg.norm(n2)
+    n3 = n3 / np.linalg.norm(n3)
+
+    return n1, n2, n3
---
+++ tests/utils/test_geometry.py
@@ -0,0 +1,124 @@
+import numpy as np
+from scipy.spatial import Delaunay
+import pytest
+
+from pydglib.utils.geometry import (
+    get_area_of_triangle,
+    get_perimeter_of_triangle,
+    get_outward_unit_normals_of_triangle,
+)
+
+
+# Valid triangles labeled with their correct surface area and perimeter
+VALID_TRIANGLES = [
+    # v1, v2, v3, area, perimeter, n1, n2, n3
+    (
+        np.array([-1, -1]),
+        np.array([1, -1]),
+        np.array([-1, 1]),
+        2,
+        4 + 2 * np.sqrt(2),
+        np.array([0, -1]),
+        np.array([1 / np.sqrt(2), 1 / np.sqrt(2)]),
+        np.array([-1, 0]),
+    ),
+    (
+        np.array([0, 0]),
+        np.array([1, 0]),
+        np.array([1, 1]),
+        0.5,
+        2 + np.sqrt(2),
+        np.array([0, -1]),
+        np.array([1, 0]),
+        np.array([-1 / np.sqrt(2), 1 / np.sqrt(2)]),
+    ),
+    (
+        np.array([1.5, 1.5]),
+        np.array([2, 2.2]),
+        np.array([0.9, 2.4]),
+        0.435,
+        3.05993189809,
+        np.array([1, -5 / 7]) / np.linalg.norm(np.array([1, -5 / 7])),
+        np.array([1, 5.5]) / np.linalg.norm(np.array([1, 5.5])),
+        np.array([-1, -2 / 3]) / np.linalg.norm(np.array([1, 2 / 3])),
+    ),
+]
+
+
+class TestGetAreaOfTriangle:
+    def test_accepts_1d_numpy_arrays(self):
+        v1 = np.array([-1, -1])
+        v2 = np.array([0, 0])
+        v3 = np.array([-1, 0])
+        assert get_area_of_triangle(v1, v2, v3) == 0.5
+
+    def test_returns_zero_if_vertices_colinear(self):
+        v1 = np.array([-1, -1])
+        v2 = np.array([0, 0])
+        v3 = np.array([1, 1])
+        assert np.isclose(get_area_of_triangle(v1, v2, v3), 0)
+
+    @pytest.mark.parametrize("v1,v2,v3,area,perimeter,n1,n2,n3", VALID_TRIANGLES)
+    def test_returns_correct_areas(self, v1, v2, v3, area, perimeter, n1, n2, n3):
+        assert np.isclose(get_area_of_triangle(v1, v2, v3), area)
+
+
+class TestGetPerimeterOfTriangle:
+    def test_accepts_1d_numpy_arrays(self):
+        v1 = np.array([-1, -1])
+        v2 = np.array([0, 0])
+        v3 = np.array([-1, 0])
+        assert np.isclose(get_perimeter_of_triangle(v1, v2, v3), 2 + np.sqrt(2))
+
+    @pytest.mark.parametrize("v1,v2,v3,area,perimeter,n1,n2,n3", VALID_TRIANGLES)
+    def test_returns_correct_perimeters(self, v1, v2, v3, area, perimeter, n1, n2, n3):
+        assert np.isclose(get_perimeter_of_triangle(v1, v2, v3), perimeter)
+
+
+class TestGetOutwardUnitNormalsOfTriangle:
+    def test_returns_three_numpy_arrays(self):
+        v1 = np.array([-1, -1])
+        v2 = np.array([0, 0])
+        v3 = np.array([-1, 0])
+        n1, n2, n3 = get_outward_unit_normals_of_triangle(v1, v2, v3)
+        for n in [n1, n2, n3]:
+            assert isinstance(n, np.ndarray)
+            assert len(n.shape) == 1
+            assert n.shape[0] == 2
+
+    def test_returned_normals_are_orthogonal_to_edges(self):
+        v1 = np.array([-1, -1])
+        v2 = np.array([0, 0])
+        v3 = np.array([-1, 0])
+        n1, n2, n3 = get_outward_unit_normals_of_triangle(v1, v2, v3)
+        assert np.isclose(np.dot(n1, v2 - v1), 0)
+        assert np.isclose(np.dot(n2, v3 - v2), 0)
+        assert np.isclose(np.dot(n3, v1 - v3), 0)
+
+    def test_returned_normals_are_unit_length(self):
+        v1 = np.array([-0.9, -1])
+        v2 = np.array([0, 0])
+        v3 = np.array([-1, 0])
+        n1, n2, n3 = get_outward_unit_normals_of_triangle(v1, v2, v3)
+        assert np.isclose(np.linalg.norm(n1), 1)
+        assert np.isclose(np.linalg.norm(n2), 1)
+        assert np.isclose(np.linalg.norm(n3), 1)
+
+    def test_returned_normals_point_outwards_from_the_triangle(self):
+        v1 = np.array([-11, -12])
+        v2 = np.array([1, 0.5])
+        v3 = np.array([-9, 2])
+        n1, n2, n3 = get_outward_unit_normals_of_triangle(v1, v2, v3)
+        tri = Delaunay(np.vstack((v1, v2, v3)))
+        assert tri.find_simplex(0.5 * (v1 + v2) + n1) == -1
+        assert tri.find_simplex(0.5 * (v2 + v3) + n2) == -1
+        assert tri.find_simplex(0.5 * (v3 + v1) + n3) == -1
+
+    @pytest.mark.parametrize("v1,v2,v3,area,perimeter,n1,n2,n3", VALID_TRIANGLES)
+    def test_returns_correct_outward_unit_normals(
+        self, v1, v2, v3, area, perimeter, n1, n2, n3
+    ):
+        normals = get_outward_unit_normals_of_triangle(v1, v2, v3)
+        assert np.allclose(normals[0], n1)
+        assert np.allclose(normals[1], n2)
+        assert np.allclose(normals[2], n3)
